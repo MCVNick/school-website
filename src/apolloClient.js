ac444033b12315d957cbf4d71d596ea622fe026a
mcvnick <mcvnick@gmail.com>
2024-08-10 00:19:55 -0600

    nicholasmcqueen.com

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -3,7 +3,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql", // Replace with your GraphQL endpoint
+  uri: "https://nicholasmcqueen.com/graphql", // Replace with your GraphQL endpoint
   cache: new InMemoryCache(),
 });
 
