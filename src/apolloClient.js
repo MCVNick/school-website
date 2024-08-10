6d595ca62a389e78719ff1bdfa35a129bc0735c7
MCVNick <earlyreaderai@gmail.com>
2024-08-10 05:27:39 +0000

    updated apolloClient.js to be an ip

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -3,7 +3,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql", // Replace with your GraphQL endpoint
+  uri: "http://68.69.162.94:4000/graphql", // Replace with your GraphQL endpoint
   cache: new InMemoryCache(),
 });
 
